1ee46d105dbc6acba6ce58d20fc813d7cebdb079
imransk444 <imran.sk444@gmail.com>
2020-03-18 23:23:08 -0500

    Hood.java

---
+++ Hood.java
@@ -0,0 +1,15 @@
+
+public class Hood {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=1;i<10;i++) {
+			 System.out.println("This is IMRAN HOOD"+i);
+		 }
+		
+		
+		
+
+	}
+
+}
